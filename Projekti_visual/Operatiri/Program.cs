a646489262feff493ff19f906e2520e870d41161
kanjo494@gmail.com <kanjo494@gmail.com>
2020-04-03 12:39:10 +0200

    Dobar Dan

---
+++ Projekti_visual/Operatiri/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Operatiri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dobar dan!");
+        }
+    }
+}
